9022ae1a5e7b522fcea4f12807c178491498b474
unknown <pasindu0996@gmail.com>
2019-05-21 11:03:23 +0530

    Created Payment Container

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -111,6 +111,80 @@ class Payment extends Component{
                     </form>
                 </div>
 
+
+
+                <div id="homepage" className="paymentBox">
+                    
+                    <form onSubmit={this.handleSubmit}>
+
+                        <ul id="horizontal-list-ppp">
+                            
+                            <li> 
+                                <label htmlFor="creditCard">
+                                    
+                                    <input type="radio" id="creditCard" name="payment" value="creditcard" checked="checked"/> 
+                                    <span>Credit Card Payment</span>
+                                    
+                                </label>
+                                
+                            </li>
+
+                            <li>
+                                <label>Card Number</label>
+                                <input type="text"/>
+
+                            </li>
+
+                            <li>
+                                <label>Expire Date</label>
+                                <input type="text" placeholder="yy/mm"/>
+
+                            </li>
+
+                            <li>
+                                <label>CSV</label>
+                                <input type="text"/>
+
+                            </li>
+
+                        </ul>
+
+
+                        <ul id="horizontal-list-ppp">
+                            
+                            <li> 
+                                <label htmlFor="mobilePayment">
+                                    
+                                    <input type="radio" name="payment" value="mobile" id="mobilePayment"/> 
+                                    <span>Mobile Payment</span>
+                                    
+                                </label>
+                                
+                            </li>
+
+                            <li>
+                                <label>Mobile Number</label>
+                                <input type="text" value={this.props.loggedUser.mobileNumber}/>
+
+                            </li>
+
+                            <li>
+                                <label>PIN</label>
+                                <input type="password"/>
+
+                            </li>
+                            
+                        </ul>
+
+                        <br/>
+
+                        <div className="container center" id="proceedContainer">
+                             <button id="proceedButton">Proceed</button>
+                        </div>
+
+                    </form>
+                </div>
+
             </div>
         )
     }
